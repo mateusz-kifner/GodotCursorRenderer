005137da89229f9d6e8aa362340a70cb9c3282ce
Mateusz Kifner <kifner-mateusz@users.noreply.github.com>
2020-03-17 00:28:15 +0100

    init

---
+++ CursorRenderer.gd
@@ -0,0 +1,70 @@
+extends Viewport
+
+# You can use CursorRenderer as singelton.
+# CursorRenderer needs custom cursor image to work. (cursor provided is trash)
+
+var texRect
+
+func render_cursor(item:Texture,num:int =-1):
+	# REQUIRED: Setup invisible TextureRect with ViewportTexture to ensure that viewport is rendered.
+	if not(get_tree().get_root().has_node("CursorViewport")):
+		texRect = TextureRect.new()
+		texRect.name = "CursorViewport"
+		texRect.texture = get_texture()
+		texRect.rect_scale = Vector2()
+		texRect.set_global_position(get_tree().get_root().get_viewport().get_mouse_position())
+		get_tree().get_root().add_child(texRect)
+	# Make TextureRect with ViewportTexture visible
+	get_tree().get_root().get_node("CursorViewport").visible = true
+	# Set position to mouse, to ensure it is on screen
+	texRect.set_global_position(get_tree().get_root().get_viewport().get_mouse_position())
+	set_clear_mode(Viewport.CLEAR_MODE_ONLY_NEXT_FRAME)
+	# Setup nodes in Viewport. (for example scene only)
+	if num >0:
+		$Control/Count.text = "x"+str(num)
+	else:
+		$Control/Count.text = ""
+	$Control/Item.texture = item
+	$Control/Count.rect_position = $Control/Item.rect_size - $Control/Count.rect_size
+
+	# Let three frames pass to make sure the screen was captured.
+	yield(get_tree(), "idle_frame")
+	yield(get_tree(), "idle_frame")
+	yield(get_tree(), "idle_frame")
+
+	# Retrieve the captured image.
+	var img = get_texture().get_data()
+
+	# Flip it on the y-axis (because it's flipped).
+	img.flip_y()
+
+	# Create a texture for it.
+	var tex = ImageTexture.new()
+	tex.create_from_image(img)
+	Input.set_custom_mouse_cursor(tex)
+	
+	# Hide TextureRect with ViewportTexture visible
+	get_tree().get_root().get_node("CursorViewport").visible = false
+	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Main.gd
@@ -0,0 +1,11 @@
+extends Node2D
+
+var keyBlue  = preload("res://keyBlue.png")
+
+# This is exapmle scene that renders Cursor with Texture keyBlue and count of 10 attached to it.
+# You can use CursorRenderer as singelton.
+# CursorRenderer needs custom cursor image to work. (cursor provided is trash)
+
+func _ready():
+	yield(get_tree(), "idle_frame") # Wait one frame to ensure that CursorRenderer is loaded
+	$CursorRenderer.render_cursor(keyBlue,10) # Render Cursor
